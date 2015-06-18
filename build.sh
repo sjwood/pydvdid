c5b67a89c2e81f0b8d20f2a7a7c32d2e1bea2b0e
sjwood <octocat@nym.hush.com>
2015-06-18 21:55:12 +0100

    Changed build script to use globstar when calling compileall

--- build.sh
+++ build.sh
@@ -13,6 +13,14 @@ function __echo_to_stderr() {
     echo "$@" 1>&2
 }
 
+# test for existence of python
+PYTHON_PATH=$(which python)
+if [ $? -ne 0 ]
+then
+    __echo_to_stderr "Python is not installed."
+    exit 1
+fi
+
 # where is this script executing?
 SCRIPT_DIRECTORY=$(readlink -f "$(dirname "$0")")
 if [ $? -ne 0 ]
@@ -21,19 +29,33 @@ then
     exit 1
 fi
 
-# construct packaged code location
-CODE_DIRECTORY="$SCRIPT_DIRECTORY/pydvdid"
-if [ $? -ne 0 ]
+# is globstar option already set?
+shopt -q globstar
+GLOBSTAR_SET=$?
+
+# set globstar option (if it has not already been)
+if [ $GLOBSTAR_SET -ne 0 ]
 then
-    __echo_to_stderr "Cannot construct code directory."
-    exit 1
+    shopt -s globstar
 fi
 
-# invoke compileall module for code location
-python -m compileall $CODE_DIRECTORY
+# invoke compileall module for all python files in the directory
+python -m compileall -f $SCRIPT_DIRECTORY/**/*.py
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]
 then
     __echo_to_stderr "Python compileall returned exit code $EXIT_CODE."
+fi
+
+# reset globstar option (if it was not originally set)
+if [ $GLOBSTAR_SET -ne 0 ]
+then
+    shopt -u globstar
+fi
+
+# exit with appropriate code
+if [ $EXIT_CODE -ne 0 ]
+then
     exit 1
 fi
+exit 0

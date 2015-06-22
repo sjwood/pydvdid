74f28f9a5b451a62af2aba6aa3d31895de68dafe
sjwood <octocat@nym.hush.com>
2015-06-22 23:48:30 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pydvdid
+
+## Overview
+
+[pydvdid][1] is a pure python implementation of the Windows API [IDvdInfo2::GetDiscID][2] method.
+
+## License
+
+[Apache License, Version 2.0][3]
+
+  [1]: https://github.com/sjwood/pydvdid
+  [2]: https://msdn.microsoft.com/en-us/library/windows/desktop/dd376453.aspx
+  [3]: https://raw.githubusercontent.com/sjwood/pydvdid/master/LICENSE
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="pydvdid",
     packages=["pydvdid"],
     version="0.1",
-    description="A pure python implementation of the Windows API IDvdInfo2::GetDiscID method",
+    description="A pure python implementation of the Windows API IDvdInfo2::GetDiscID method.",
     url="https://github.com/sjwood/pydvdid",
     author="Steve Wood",
     author_email="octocat@nym.hush.com",

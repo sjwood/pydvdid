e5c0ca545ebd41abfef32c6953edc5345fb7c8ee
sjwood <octocat@nym.hush.com>
2018-01-24 01:00:26 +0000

    adjusted name to satisfy pylint

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from setuptools import setup
 
 
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+    README = readme_file.read()
 
 
 setup(
     name="pydvdid",
     version="1.1",
     description="A pure Python implementation of the Windows API IDvdInfo2::GetDiscID method, as used by Windows Media Center to compute a 'practically unique' 64-bit CRC for metadata retrieval.", # pylint: disable=locally-disabled, line-too-long
-    long_description=readme,
+    long_description=README,
     author="Steve Wood",
     author_email="octocat@nym.hush.com",
     url="https://github.com/sjwood/pydvdid",
